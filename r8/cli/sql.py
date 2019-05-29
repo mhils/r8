f4f7d33b3cc6e263032661f6b30a09d72958cad4
Maximilian Hils <git@maximilianhils.com>
2019-05-29 17:10:23 +0200

    don't delete flags via ON DELETE CASCADE
    Deletions aren't deferred, so we run into really nasty errors here.

--- r8/cli/sql.py
+++ r8/cli/sql.py
@@ -46,7 +46,7 @@ def init(origin, static_dir, listen_address, database) -> None:
             fid TEXT PRIMARY KEY NOT NULL,
             cid TEXT NOT NULL,
             max_submissions INTEGER NOT NULL,
-            FOREIGN KEY (cid) REFERENCES challenges(cid) ON DELETE CASCADE
+            FOREIGN KEY (cid) REFERENCES challenges(cid)
         );
         CREATE TABLE submissions (
             uid TEXT NOT NULL,
