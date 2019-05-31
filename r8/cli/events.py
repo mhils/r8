7d6fd1aa34ea409e3c94d478cd8cc32e9ff206ba
Maximilian Hils <git@maximilianhils.com>
2019-05-31 13:56:12 +0200

    improve event output

--- r8/cli/events.py
+++ r8/cli/events.py
@@ -1,9 +1,24 @@
 import time
+from typing import List, Optional
 
 import click
 
 import r8
 from r8 import util
+
+
+def min_distinguishable_column_width(elements: List[str]) -> int:
+    elements = set(elements)
+    if not elements:
+        return 0
+
+    n = max(len(x) for x in elements)
+    while n > 0:
+        if len(set(x[:n] for x in elements)) == len(elements):
+            n -= 1
+        else:
+            break
+    return n + 1
 
 
 @click.command("events")
@@ -20,20 +35,39 @@ def cli(rows, watch, query):
     """
     query = " ".join(query)
 
-    def print_event(event):
-        time, ip, uid, cid, type, data = event
-        ip = ip.rjust(15)
-        uid = (uid or "-").ljust(8)
-        type = type.ljust(20)
-        cid = cid or "-"
-        cid_w = max(20, len(cid))
-        w, h = click.get_terminal_size()
-        data_w = w - len(time) - 15 - 8 - 20 - cid_w - 5
+    time_w = 19
+    ip_w = 15
+    uid_w = min_distinguishable_column_width(r8.util.get_users())
+    uid_w = max(10, min(uid_w, 25))
+    type_w = 20
+    with r8.db:
+        cids = [x[0] for x in
+                r8.db.execute("SELECT cid FROM challenges").fetchall()]
+    cid_w = min_distinguishable_column_width(cids)
+    cid_w = max(10, min(uid_w, 25))
+
+    def print_event(
+            time: str,
+            ip: str,
+            type: str,
+            data: Optional[str],
+            cid: Optional[str],
+            uid: Optional[str]
+    ):
+        time = time[:time_w].ljust(time_w)
+        ip = ip[:ip_w].rjust(ip_w)
+        type = type[:type_w].ljust(type_w)
+
+        cid = (cid or "-")[:cid_w].ljust(cid_w)
+        uid = (uid or "-")[:uid_w].ljust(uid_w)
+
+        total_w, _ = click.get_terminal_size()
+        data_w = total_w - time_w - ip_w - type_w - cid_w - uid_w - 5
         if data:
             data = r8.util.console_escape(data)
         else:
             data = "-"
-        data = (data[:data_w]).ljust(data_w)
+        data = data[:data_w].ljust(data_w)
         print(time, ip, uid, type, data, cid)
 
     with r8.db:
@@ -41,14 +75,14 @@ def cli(rows, watch, query):
     while True:
         with r8.db:
             new = r8.db.execute(f"""
-            SELECT time, ip, uid, cid, type, data
+            SELECT time, ip, type, data, cid, uid
             FROM events
             {query}
             ORDER BY rowid
             LIMIT 100 OFFSET {seen}""").fetchall()
             seen += len(new)
             for e in new:
-                print_event(e)
+                print_event(*e)
             if not watch:
                 break
             time.sleep(0.5)
